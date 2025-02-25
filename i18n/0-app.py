a54df82538ddfe511fec9e8f7951f0a57d40b23c
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2025-02-25 14:34:00 +0200

    Remove unnecessary whitespace and improve code formatting in 0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -8,8 +8,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+    
+
 def index():
+    
+
+
     """hello world"""
+    
+
     return render_template("0-index.html", message="Hello world")
 
 if __name__ == "__main__":
