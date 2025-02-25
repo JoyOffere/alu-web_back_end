0a39204ddb67715e1b9aeed5196d092b2517ff5e
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2025-02-25 14:20:03 +0200

    Flask installed

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """hello world"""
-    return render_template("0-index.html", message="Welcome to Holberton")
+    return render_template("0-index.html", message="Hello world")
+
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-    
